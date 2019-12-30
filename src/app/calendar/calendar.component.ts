e35a75a8819cd1adfb89650f4a78a6753d576410
Fegrant <33790526+Fegrant@users.noreply.github.com>
2019-12-30 19:09:25 -0300

    Removed subject filter and made some graphic changes

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,17 +1,37 @@
 import { Component, OnInit, ViewChildren, QueryList, ChangeDetectorRef } from '@angular/core';
+/*
 import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDropList } from '@angular/cdk/drag-drop';
 import { Observable } from 'rxjs';
+*/
 import { MatAutocompleteSelectedEvent } from '@angular/material';
+import { Time } from '@angular/common';
 
-interface Subject {
-  name: string;
-  color?: string;
-  division?: string;
-}
-
+/*
 interface WeekDay {
   name: string;
   subjects: Subject[];
+}
+*/
+/* "Dummy" datatypes to simulate obtained data through the algorithm */
+interface possibleSchedules {
+  weight?: number;          //  Both datatypes won´t be used in current version of the calendar, so they are optional
+  priorities?: string[];    //
+  subjects: Subject[];
+}
+
+interface Subject {
+  name: string;
+  teachers?: string;        // Same as above
+  commissionName: string;
+  color: string;
+  commissionTimes?: CommissionTime[];
+}
+
+interface CommissionTime {
+  day?: string;             // Same as above
+  location?: string;        //
+  initialHour: Time;
+  finalHour: Time;
 }
 
 @Component({
@@ -19,71 +39,62 @@ interface WeekDay {
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
+
 export class CalendarComponent implements OnInit {
 
-  weekDays: WeekDay[] = [
-    { name: 'Lunes', subjects: [
-      { name: 'Algebra' },
-      { name: 'Mate 1' },
-      { name: 'Fisica' },
-      { name: 'PI' }
-    ]},
-    { name: 'Martes', subjects: [
-      { name: 'Algebra' },
-      { name: 'Mate 1' },
-      { name: 'Fisica' },
-      { name: 'PI' }
-    ] },
-    { name: 'Miercoles', subjects: [
-      { name: 'Algebra' },
-      { name: 'Mate 1' },
-      { name: 'Fisica' },
-      { name: 'PI' }
-    ] },
-    { name: 'Jueves', subjects: [
-      { name: 'Algebra' },
-      { name: 'Mate 1' },
-      { name: 'Fisica' },
-      { name: 'PI' }
-    ] },
-    { name: 'Viernes', subjects: [
-      { name: 'Algebra' },
-      { name: 'Mate 1' },
-      { name: 'Fisica' },
-      { name: 'PI' }
-    ] }
-  ];
+  schedules: possibleSchedules[] = [
+    {
+      subjects: [
+        { name: 'Algebra', color: '#435332', commissionName: 'Comision 2' },
+        { name: 'Matematica 1', color: '#123', commissionName: 'Comision 1' },
+        { name: 'Fisica', color: '#adc242', commissionName: 'Comision 3' },
+        { name: 'PI', color: '#FFaaFF', commissionName: 'Comision 4' }       
+      ]
+    }
+  ]
 
   days = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes']
   hours: string[] = [];
-  selectedSubjects: Subject[] = [];
+  
+  // Code not used atm
+  /* selectedSubjects: Subject[] = [{ name: 'Algebra', color: '#435332', commissionName: 'Comision 2'}];
   availableSubjects: Subject[] = [
-    { name: 'Matematica 1', color: '#123', division: 'Comision 1' },
-    { name: 'Algebra', color: '#435332', division: 'Comision 2' },
-    { name: 'Fisica', color: '#adc242', division: 'Comision 3' },
-    { name: 'PI', color: '#FFaaFF', division: 'Comision 4' },
+    { name: 'Matematica 1', color: '#123', commissionName: 'Comision 1' },
+    { name: 'Algebra', color: '#435332', commissionName: 'Comision 2' },
+    { name: 'Fisica', color: '#adc242', commissionName: 'Comision 3' },
+    { name: 'PI', color: '#FFaaFF', commissionName: 'Comision 4' },
   ];
+  */
   filteredOptions: Subject[] = [];
   subjectChooserValue: string = '';
+  randomNumbersGenerated: number[] = [];
+  checkedStatus = true;
 
   constructor(private cd: ChangeDetectorRef) { }
 
   ngOnInit() {
-    for (let x = 8; x < 22; x++) {
-      this.hours.push(`${x}:00`);
+    for (let x = 8; x < 22; x+=0.5) {
+      if( x % 2 == 0 || x % 2 == 1)
+        this.hours.push(`${x}:00`);
+      else
+        this.hours.push(`${Math.floor(x)}:30`)
+    }
+    for (let i = 0; i < 70; i++) {
+      this.randomNumbersGenerated.push(Math.floor(Math.random()*4));
     }
   }
 
   subjectOn(day: string, hour: string): Subject[] {
-    return [
-      { name: 'Matematica 1', color: '#123', division: 'Catedra 1' }
-    ]
+    return [this.schedules[0].subjects[0]];       // Returnes a subject to plot on the whole calendar. Used for testing  
   }
-
+  
+  // Code not used atm
+  /*
   displayFn(subject?: Subject): string | undefined {
     return subject ? subject.name : undefined;
   }
-
+  
+  
   onSubjectChooserChange() {
     this.filteredOptions = this.availableSubjects.filter(subj => 
       subj.name.toLowerCase().startsWith(this.subjectChooserValue.toLowerCase())
@@ -98,9 +109,5 @@ export class CalendarComponent implements OnInit {
     this.onSubjectChooserChange();
     this.cd.detectChanges();
   }
-
-  // Returns a random subject out of the possible 4. Used for testing before the actual subjects are implemented
-  randomSubject(): Subject[]  {
-    return [this.availableSubjects[Math.floor(Math.random() * 4)]];
-  }
+  */
 }
